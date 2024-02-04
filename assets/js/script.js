9a8ff9bafbdecdc71579ce7f4c4fd15a12cf1e30
Patricio Barahona-Gallardo <p.barahonagallardo@gmail.com>
2024-02-04 18:18:46 -0300

    Contact section implementation
    Bootstrap form implemented in row
    Bootstrap JS tooltip implemented in form button
    jQuery success alert implemented in form button
    Bootstrap responsive styling optimization

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,12 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+// Form feedback alert with jQuery
+$(document).ready(function () {
+  $("#submit").on("click", function () {
+    alert(
+      "Hemos recibido su mensaje.\nNos pondremos en contacto con usted a la brevedad."
+    );
+  });
+});
